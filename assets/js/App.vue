c648e49e91350b412193ea4b1c43373086a11f64
Josh Nussbaum <joshnuss@gmail.com>
2018-12-15 01:22:31 -0500

    Adds example component

--- assets/js/App.vue
+++ assets/js/App.vue
@@ -1,20 +1,25 @@
 <template>
-  <div>Hello from {{name}}!</div>
+  <Title>Hello from {{name}}!</Title>
 </template>
 
 <script>
+import Title from './components/Title.vue';
+
 export default {
   name: 'App',
   data() {
     return {
       name: "Vue"
     }
+  },
+  components: {
+    Title
   }
 };
 </script>
 
 <style scoped>
-  div {
+  h1 {
     background: pink;
     text-align: center;
     padding: 2em;
---
+++ assets/js/components/Title.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>
+    <slot/>
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'Title'
+};
+</script>
