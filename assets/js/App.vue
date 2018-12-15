1cca4b50d008fb794a67f4aa97cacd5cafc8e8a3
Josh Nussbaum <joshnuss@gmail.com>
2018-12-15 01:13:15 -0500

    Adds Vue application

---
+++ assets/js/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>Hello from {{name}}!</div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      name: "Vue"
+    }
+  }
+};
+</script>
+
+<style scoped>
+  div {
+    background: pink;
+    text-align: center;
+    padding: 2em;
+  }
+</style>
