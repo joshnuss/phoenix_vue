4bf36f188704569daab30af9d0e624123cb1e3fc
Josh Nussbaum <joshnuss@gmail.com>
2018-12-16 00:49:11 -0500

    Adds basic layout

--- assets/js/App.vue
+++ assets/js/App.vue
@@ -1,9 +1,58 @@
 <template>
-  <router-view></router-view>
+  <v-app>
+    <v-navigation-drawer app v-model="drawer" dark>
+      <v-list>
+        <v-list-tile to="/" @click="toggleDrawer">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title >Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile to="/contact" @click="toggleDrawer">
+          <v-list-tile-action>
+            <v-icon>email</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title >Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app>
+      <v-toolbar-side-icon @click.stop="toggleDrawer()"></v-toolbar-side-icon>
+      <v-toolbar-title>
+        Phoenix.Vue
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn icon to="/contact">
+          <v-icon>email</v-icon>
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+    <v-footer app></v-footer>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      drawer: false,
+    }
+  },
+  methods: {
+    toggleDrawer() {
+      this.drawer = !this.drawer
+    }
+  }
 }
 </script>
